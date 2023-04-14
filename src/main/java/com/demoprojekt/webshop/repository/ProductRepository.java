b9bce6b85f445bed53d782f088e56c585f396eb1
jakwinhub <jakwin@gmx.de>
2023-04-14 11:07:48 +0200

    Add ProductController, ProductRepository, ProductResponse

--- src/main/java/com/demoprojekt/webshop/repository/ProductRepository.java
+++ src/main/java/com/demoprojekt/webshop/repository/ProductRepository.java
@@ -2,49 +2,47 @@ package com.demoprojekt.webshop.repository;
 
 import com.demoprojekt.webshop.model.ProductResponse;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductRepository {
+    List<ProductResponse> products = Arrays.asList(
+            new ProductResponse(
+                    "1",
+                    "AMD Ryzen 9 5950 X",
+                    "Lorem Ipsum",
+                    79900,
+                    Arrays.asList("AMD", "Processor")),
+            new ProductResponse(
+                    "2",
+                    "Intel Core i9-9900KF",
+                    "Lorem Ipsum",
+                    37900,
+                    Arrays.asList("Intel", "Processor")),
+            new ProductResponse(
+                    "3",
+                    "NVIDIA GeForece GTX 1080 Ti Black Edition",
+                    "Lorem Ipsum",
+                    74900,
+                    Arrays.asList("NVIDIA", "Graphics"))
+    );
+
     public List<ProductResponse> findAll(String tag) {
-
-        List<ProductResponse> products = Arrays.asList(
-                new ProductResponse(
-                        "1",
-                        "AMD Ryzen 9 5950 X",
-                        "Lorem Ipsum",
-                        79900,
-                        Arrays.asList("AMD", "Processor")),
-                new ProductResponse(
-                        "2",
-                        "Intel Core i9-9900KF",
-                        "Lorem Ipsum",
-                        37900,
-                        Arrays.asList("Intel", "Processor")),
-                new ProductResponse(
-                        "3",
-                        "NVIDIA GeForece GTX 1080 Ti Black Edition",
-                        "Lorem Ipsum",
-                        74900,
-                        Arrays.asList("NVIDIA", "Graphics"))
-        );
-
 
         if (tag == null)
             return products;
         else {
-            tag = tag.toLowerCase();
-            List<ProductResponse> filtered = new ArrayList<>();
-            for (ProductResponse p : products) {
-                List<String> lowercaseTags = new ArrayList<>();
-                for (String t : p.getTags()) {
-                    lowercaseTags.add(t.toLowerCase());
-                }
-                if (lowercaseTags.contains(tag))
-                    filtered.add(p);
-            }
-            return filtered;
+            String lowercaseTag = tag.toLowerCase();
+
+            return products.stream()
+                    .filter(p -> lowercaseTags(p).contains(lowercaseTag))
+                    .collect(Collectors.toList());
         }
+    }
+
+    private List<String> lowercaseTags(ProductResponse p) {
+        List<String> tags = p.getTags();
+        return tags.stream().map(String::toLowerCase).collect(Collectors.toList());
     }
 }
