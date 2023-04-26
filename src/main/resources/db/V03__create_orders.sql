d8d37306d7b3e57e4c2881bb3d1d6e745e03c826
jakwinhub <jakwin@gmx.de>
2023-04-26 13:31:27 +0200

    Create database migration scripts

---
+++ src/main/resources/db/V03__create_orders.sql
@@ -0,0 +1,32 @@
+create table order_entity_order_positions
+(
+    order_entity_id varchar(36) not null,
+    id              varchar(36) not null,
+    order_id        varchar(36) not null,
+    product_id      varchar(36) not null,
+    quantity        bigint      not null
+);
+
+create table orders
+(
+    id          varchar(36)  not null,
+    customer_id varchar(36)  not null,
+    order_time  timestamp(6) not null,
+    status      varchar(255) not null,
+    primary key (id)
+);
+
+alter table if exists order_entity_order_positions
+    add constraint order_positions_order_id_fk
+        foreign key (order_entity_id)
+            references orders;
+
+alter table if exists order_entity_order_positions
+    add constraint order_positions_order_id_fk
+        foreign key (product_id)
+            references products;
+
+alter table if exists orders
+    add constraint orders_customer_id_fk
+        foreign key (customer_id)
+            references customers;
